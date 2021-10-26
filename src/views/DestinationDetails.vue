ebbda242ea154eb76c3631fdd4c561258b251131
Canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-26 13:32:07 +0300

    hash add, css

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -13,7 +13,7 @@
     </section>
     <section class="experiences">
       <h2>Top experiences in {{ destination.name }}</h2>
-      <div class="cards">
+      <div class="cards" id="experience">
         <div
           v-for="experience in destination.experiences"
           :key="experience.slug"
@@ -23,6 +23,7 @@
             :to="{
               name: 'experienceDetails',
               params: { experienceSlug: experience.slug },
+              hash: '#experience',
             }"
           >
             <img
@@ -72,6 +73,11 @@ img {
   width: 100%;
   max-height: 400px;
 }
+
+.experiences {
+  padding: 40px 0;
+}
+
 .destination-details {
   display: flex;
   justify-content: space-between;
